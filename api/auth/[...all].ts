51e41f9b1080b1fe73fd9824ef2c4eb4d7124963
LydonDev <hi.lydon@gmail.com>
2025-10-02 14:36:47 +0100

    auth api fix

--- api/auth/[...all].ts
+++ api/auth/[...all].ts
@@ -5,8 +5,8 @@ export const config = { api: { bodyParser: false } };
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    const authModule = await import("../../lib/auth");
-    const nodeHandler = toNodeHandler(authModule.auth.handler);
+    const { auth } = await import("../../lib/auth");
+    const nodeHandler = toNodeHandler(auth);
     return nodeHandler(req, res);
   } catch (err) {
     console.error("Auth handler error:", err);
